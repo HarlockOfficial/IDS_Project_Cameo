69bff9903db1f449e7f55ca81b00247e99da3848
HarlockOfficial <harlockofficial.github@gmail.com>
2021-10-26 18:47:29 +0200

    Project Skeleton

---
+++ src/main/java/cameo/impianto_balneare/IdsProjectCameoApplication.java
@@ -0,0 +1,13 @@
+package cameo.impianto_balneare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdsProjectCameoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdsProjectCameoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cameo/impianto_balneare/IdsProjectCameoApplicationTests.java
@@ -0,0 +1,13 @@
+package cameo.impianto_balneare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdsProjectCameoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
