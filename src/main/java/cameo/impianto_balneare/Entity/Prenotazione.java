87d39f5024036a1a697f23ae5dd65e6110f1fe6a
alessandro <alessandro.quercetti@gmail.com>
2021-12-17 18:51:21 +0100

    eliminazione lettino e sdraio

--- src/main/java/cameo/impianto_balneare/Entity/Prenotazione.java
+++ src/main/java/cameo/impianto_balneare/Entity/Prenotazione.java
@@ -1,6 +1,7 @@
 package cameo.impianto_balneare.Entity;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,6 +17,20 @@ public class Prenotazione {
     @JoinTable(name = "ombrelloni_list",joinColumns = @JoinColumn (name = "ombrellone_id"),
     inverseJoinColumns = @JoinColumn (name = "id"))
     private List<Ombrellone> ombrellone;
+
+    //todo vedi bene queste cose aggiunte
+    @Column
+    private int lettiniNumber;
+
+    @Column
+    private int sdraioNumber;
+
+    @Column
+    private Date dataPrenotazione;
+
+    //aggiungi la correlazione con la tabella ;) manytoone o onetomany nun sacc quale mettere
+    @Column
+    private User user;
 
 
 }
