2d8ff984f33b3f0fb99c2e9e7d8440cfb84c5e0e
HarlockOfficial <harlockofficial.github@gmail.com>
2022-01-18 12:55:23 +0100

    added cors

--- backend/src/main/java/cameo/impianto_balneare/configs/SecurityConfig.java
+++ backend/src/main/java/cameo/impianto_balneare/configs/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -11,6 +12,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity security) throws Exception
     {
+        getHttp().cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
         security.httpBasic().disable();
     }
 }
